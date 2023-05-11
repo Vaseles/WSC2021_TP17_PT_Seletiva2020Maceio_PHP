284605617eb04ac5e82475484e3b438c7fe2c629
vaseles <vaselesstolarov813@gmail.com>
2023-05-11 18:26:00 +0600

    add motherboard adding

--- front/src/pages/Create/Create.jsx
+++ front/src/pages/Create/Create.jsx
@@ -6,6 +6,7 @@ import styles from './Create.module.css'
 import style from './../Home/Home.module.css'
 import axios from 'axios'
 import { useAuth } from '../../hooks/useAuth'
+import Product from '../../components/Product/Product'
 
 
 const Create = () => {
@@ -13,15 +14,25 @@ const Create = () => {
   const {isAuth, tokenAuth} = useAuth()
   const [content, setContent] = useState([])
   const category__list = ['motherboards', 'processors', 'rammemories', 'power_supplies', 'storagedevices', 'graphiccards']
+  const [error, setError] = useState()
 
-  const [machine, setMachine] = useState([])
+  // Massive for create machine
+  const [machine, setMachine] = useState({
+    motherboards: {}, 
+    processors: {}, 
+    rammemories: {},
+    rammemories_count: 0,
+    power_supplies: {},
+    graphiccards: {},
+    storagedevices: {},
+  })
 
   useEffect(() => {
-    sortFunction(category)
-    document.title = 'Create'
+    sortFunction(category) // auto category loading
+    document.title = 'Create' // retitle
   }, [])
 
-  // show value when u selected some resuls and write in localStorage
+  // ! show value when u selected some resuls and write in localStorage
   const categoryShow = (e) => {
     sortFunction(e.target.value)
     
@@ -29,7 +40,7 @@ const Create = () => {
     setCategory(e.target.value)
   }
 
-  // sort by category
+  // ! sort by category
   const sortFunction = (cat) => {
     axios.get(`http://127.0.0.1:8000/api/search?category=${cat}&q=I`, {
       headers: { 
@@ -41,8 +52,49 @@ const Create = () => {
       })
   }
 
+  // ! Input Change
+  const handleInputChange = (e) => {
+    const {name, value} = e.target
+    setMachine(preValues => ({
+      ...preValues, [name]: value
+    }))
+  }
+
+  // ! Drag and drop
+  const handleDragStart = (e, product) => {
+    e.dataTransfer.setData('text/plain', JSON.stringify(product))
+  }
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault()
+    const {name} = e.target
+    console.log(name, category)
+    if (name  === category) {
+      let product = e.dataTransfer.getData('text')
+      console.log(product)
+
+      setMachine(preValues => ({
+        ...preValues, [name]: JSON.parse(product)
+      }))
+
+    } else {
+      e.target.className = 'textarea__error'
+      setError(`You can't put ${category} in ${name}`)
+      
+      setTimeout(() => {
+        e.target.className = ''
+      }, 1000)
+    }
+
+  }
+
+  // !  Add New Machine
   const create = () => {
-    setMachine()
+    console.log(machine)
   }
 
   return (
@@ -50,6 +102,7 @@ const Create = () => {
       <div className={styles.create} >
         <div className={styles.create__header}>
           <h3>Add A New Car</h3>
+          {error?<div className={styles.error}>{error}</div>: <></>}
           <Button onClick={create} >Save</Button>
         </div>
       </div>
@@ -58,17 +111,17 @@ const Create = () => {
           <select name="" id="" onChange={categoryShow} value={category}>
             <option value="" disabled selected>Choose Object</option>
             {category__list.map(category__list => 
-              <option value={category__list}>{category__list}</option>  
+              <option key={category__list.id}  value={category__list}>{category__list}</option>  
             )}
           </select>
           <div className={styles.create__body__right__elements}>
           {content? (
           <div className={style.products}>
-            {content.map(product => 
-                <div className={styles.product} draggable={true}>
+            {content.map((product, item) => 
+                <div key={product.id} className={styles.product} draggable onDragStart={(e) => handleDragStart(e, product)}>
                   <img src={`http://127.0.0.1:8000/images/${product.imageUrl}.png`} alt="" draggable={false} />
                   <div className={styles.product__item} draggable={false} >
-                    <a href="" draggable={false}>{product.name}</a>
+                    <a href="" draggable={false}>{product.id}. {product.name}</a>
                     <div className={styles.product__notification} >
                       <h5>{product.name}</h5>
                       <h3> {product.brandId? (<>Brand: {product.brandId}</>) :( <></> ) }</h3>
@@ -82,24 +135,35 @@ const Create = () => {
           </div>
         </div>
         <form className={styles.create__body__left}>
-          <div className={styles.card}>
+
+          {/* motherboard */}
+          <div className={styles.card} >
             <div className={styles.card__header}>
               <h3>MotherBoard</h3>
               {/* <Button>delete</Button> */}
             </div>
-            <div className={styles.card__body}>
-              <input type="text"   />
-              <h2>+</h2>
+            <div   className={styles.card__body}  >
+                  <textarea name="motherboards" onDragOver={handleDragOver} onDrop={handleDrop} ></textarea>
+             {machine.motherboards ? (
+                <Product product={machine.motherboards} />
+             ): <></>}
+            <h2>+</h2> 
             </div>
           </div>
 
+            {/* processor */}
           <div className={styles.card}>
             <div className={styles.card__header}>
               <h3>Processor</h3>
               {/* <Button>delete</Button> */}
             </div>
             <div className={styles.card__body}>
-              <input type="text"   />
+            <textarea 
+                name='processors' 
+                // onChange ={handleInputChange} 
+                // value={machine.processors}
+                onDragOver={handleDragOver} onDrop={handleDrop}
+             ></textarea>
               <h2>+</h2>
             </div>
           </div>
@@ -107,11 +171,23 @@ const Create = () => {
           <div className={styles.card}>
             <div className={styles.card__header}>
               <h3>Memory Ram</h3>
-              <input type="number" placeholder='count...'  />
+              <input 
+                type="number" 
+                name='rammemories_count' 
+                placeholder='count...' 
+                // onChange={handleInputChange} 
+                onDragOver={handleDragOver} onDrop={handleDrop}
+                // value={machine.rammemories.count}  
+              />
               {/* <Button>delete</Button> */}
             </div>
             <div className={styles.card__body}>
-              <input type="text" />
+            <textarea 
+                name='rammemories' 
+                // onChange ={handleInputChange} 
+                onDragOver={handleDragOver} onDrop={handleDrop}
+                // value={machine.rammemories}
+             ></textarea>
               <h2>+</h2>
             </div>
           </div>
@@ -122,18 +198,28 @@ const Create = () => {
               {/* <Button>delete</Button> */}
             </div>
             <div className={styles.card__body}>
-              <input type="text"   />
+             <textarea
+                name='power_supplies' 
+                // onChange ={handleInputChange} 
+                onDragOver={handleDragOver} onDrop={handleDrop}
+                // value={machine.power_supplies}
+             ></textarea>
               <h2>+</h2>
             </div>
           </div>
 
           <div className={styles.card}>
             <div className={styles.card__header}>
-              <h3>Ram Memory</h3>
+              <h3>Graphic Card</h3>
               {/* <Button>delete</Button> */}
             </div>
             <div className={styles.card__body}>
-              <input type="text"   />
+             <textarea
+                name='graphiccards' 
+                // onChange ={handleInputChange} 
+                onDragOver={handleDragOver} onDrop={handleDrop}
+                // value={machine.graphiccards}
+             ></textarea>
               <h2>+</h2>
             </div>
           </div>
@@ -144,7 +230,7 @@ const Create = () => {
               {/* <Button>delete</Button> */}
             </div>
             <div className={styles.card__body}>
-              <input type="text"   />
+             <textarea></textarea>
               <h2>+</h2>
             </div>
           </div>
